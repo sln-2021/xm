2e731632d02a073150a480e66c2b0f0174527036
sunlina <jxxpglll@163.com>
2021-06-16 18:22:10 +0800

    去掉li标签的下划线

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Header from './components/header/index';
-
+import Content from './components/content/index';
 function App() {
   return (
     <>
       <Header></Header>
+      <Content></Content>
     </>
   );
 }
